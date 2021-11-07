c009622aebd0fe49e663c9bd2cf3bf20900ef972
SARA-MARIA SUPLEANTU <sara.supleantu@stud.ubbcluj.ro>
2021-11-07 17:57:51 +0200

    Add project files.

---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Supleantu_SaraMaria_Lab5.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Expense> Expense { get; set; }
+    }
+}
